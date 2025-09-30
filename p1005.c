f1cdffef88e1cf92e8340540eebfd17eb9979155
Tairikuookami <88234821+GrayWolf64@users.noreply.github.com>
2025-09-30 13:40:57 +0800

    Create p1005.c

---
+++ p1005.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        printf("%d\n", gcd(a, b));
+    }
+
+    return 0;
+}
