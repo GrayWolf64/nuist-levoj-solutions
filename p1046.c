1ad5cc593dac00dc5a28f9fc50470b640fd90d4d
Tairikuookami <88234821+GrayWolf64@users.noreply.github.com>
2025-10-03 13:34:45 +0800

    Create p1046.c

---
+++ p1046.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int k;
+        scanf("%d", &k);
+
+        if (i == (n - 1) / 2) {
+            printf("%d\n", k);
+            break;
+        }
+    }
+
+    return 0;
+}
