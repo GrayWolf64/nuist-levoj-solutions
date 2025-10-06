a5551174a7da90c6788be808d8bf3f4f87c555a3
Tairikuookami <88234821+GrayWolf64@users.noreply.github.com>
2025-10-06 17:09:50 +0800

    Update p1004.c

--- p1004.c
+++ p1004.c
@@ -11,35 +11,29 @@ void str_num_add(char a[], char b[]) {
     int len_max = (len_a > len_b) ? len_a : len_b;
     int digits[len_max + 1];
 
-    for (int i = 0; i < len_max + 1; i++) {
-        digits[i] = 0;
-    }
+    for (int i = 0; i < len_max + 1; i++) digits[i] = 0;
 
-    unsigned short prev_carry = 0;
-    for (int i = len_a - 1, j = len_b - 1, k = 1; ; --i, --j, ++k) {
-        if (k > len_max + 1) break;
+    int prev_carry = 0;
+    for (int i = len_a - 1, j = len_b - 1, k = 1; k <= len_max + 1; i--, j--, k++) {
         int d_a = (i >= 0) ? a[i] - '0' : 0;
         int d_b = (j >= 0) ? b[j] - '0' : 0;
 
         int d_sum = d_a + d_b + prev_carry;
-        int digit = 0;
 
         if (d_sum > 9) {
-            digit = d_sum - 10;
+            digits[len_max + 1 - k] = d_sum - 10;
             prev_carry = 1;
         } else {
-            digit = d_sum;
+            digits[len_max + 1 - k] = d_sum;
             prev_carry = 0;
         }
-
-        digits[len_max - k + 1] = digit;
     }
 
-    for (int i = 0; i < len_max + 1; ++i) {
-        if (!(i == 0 && len_max > 1 && digits[i] == 0)) {
-            printf("%d", digits[i]);
-        }
+    int start = (digits[0] == 0) ? 1 : 0;
+    for (int i = start; i < len_max + 1; ++i) {
+        printf("%d", digits[i]);
     }
+    printf("\n");
 }
 
 int main() {
