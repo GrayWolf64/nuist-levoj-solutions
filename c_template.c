afcec4482bd34282aa77d0ddeee9062ade086b24
GrayWolf <tairikuookami@foxmail.com>
2025-10-18 11:38:30 +0800

    Update

--- c_template.c
+++ c_template.c
@@ -2,6 +2,7 @@
 
 int main() {
     printf("LeverageOJ\n");
+    printf("%.6f\n", 3.1415926);
 
     return 0;
 }
--- cpp_template.cpp
+++ cpp_template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
     cout << "LeverageOJ" << endl;
+    cout << fixed << setprecision(6) << 3.1415926 << endl;
 
     return 0;
 }
